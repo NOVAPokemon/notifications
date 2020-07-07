597750797f5cae2edf934562c3d581ba3593bb38
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-07 17:00:52 +0100

    add retry cycle for notification sending

--- kafka/producer.go
+++ kafka/producer.go
@@ -17,28 +17,34 @@ type NotificationsProducer struct {
 
 // IssueOneNotification send a notification
 func (nc *NotificationsProducer) IssueOneNotification(notification notificationMessages.NotificationMessage) error {
+	retry := false
 
-	w := kafka.NewWriter(kafka.WriterConfig{
-		BatchSize:    1,
-		BatchTimeout: 10 * time.Millisecond,
-		Brokers:      []string{nc.KafkaUrl},
-		Topic:        nc.Username,
-		Balancer:     &kafka.LeastBytes{},
-	})
+	for retry {
+		w := kafka.NewWriter(kafka.WriterConfig{
+			BatchSize:    1,
+			BatchTimeout: 10 * time.Millisecond,
+			Brokers:      []string{nc.KafkaUrl},
+			Topic:        nc.Username,
+			Balancer:     &kafka.LeastBytes{},
+		})
 
-	toSend := kafka.Message{
-		Key:   []byte(nc.Username),
-		Value: []byte(notification.SerializeToWSMessage().Serialize()),
-	}
+		toSend := kafka.Message{
+			Key:   []byte(nc.Username),
+			Value: []byte(notification.SerializeToWSMessage().Serialize()),
+		}
 
-	if err := w.WriteMessages(context.Background(), toSend); err != nil {
-		err = wrapPublishMessageError(err)
-		log.Error(wrapProducerError(err))
-	}
+		if err := w.WriteMessages(context.Background(), toSend); err != nil {
+			err = wrapPublishMessageError(err)
+			log.Warn(wrapProducerError(err))
+			retry = true
+		} else {
+			retry = false
+		}
 
-	if err := w.Close(); err != nil {
-		log.Error(wrapProducerError(err))
-		return err
+		if err := w.Close(); err != nil {
+			log.Error(wrapProducerError(err))
+			return err
+		}
 	}
 
 	return nil
